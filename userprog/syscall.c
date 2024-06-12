6508b50db1e5010143995797ebeb1c9b69f215cb
park-yina <kln99988@gmail.com>
2024-06-12 20:32:13 +0900

    syscall.c의 close munmap수정

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -176,16 +176,15 @@ void syscall_handler(struct intr_frame *f UNUSED)
 		close(f->R.rdi);
 		break;
 	case SYS_MMAP:
-        f->R.rax = mmap(f->R.rdi, f->R.rsi, f->R.rdx, f->R.r10, f->R.r8);
-        break;
-
-    case SYS_MUNMAP:
-        munmap(f->R.rdi);
-        break;
-
-    default:
-        break;
-    }
+		f->R.rax = mmap(f->R.rdi, f->R.rsi, f->R.rdx, f->R.r10, f->R.r8);
+		break;
+	case SYS_MUNMAP:
+		munmap(f->R.rdi);
+		break;
+	default:
+		exit(-1);
+		break;
+	}
 	// thread_exit ();
 }
 
